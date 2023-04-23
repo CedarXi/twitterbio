6915b3c970ca4f9a323d88fd7fa21908939a3b35
cedar_xi <451991124@qq.com>
2023-04-23 15:12:21 +0800

    修改部分代码

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -20,13 +20,8 @@ const handler = async (req: Request): Promise<Response> => {
   const payload: OpenAIStreamPayload = {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: prompt }],
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
     max_tokens: 200,
     stream: true,
-    n: 1,
   };
 
   const stream = await OpenAIStream(payload);
